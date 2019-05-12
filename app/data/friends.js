1485bcc7e2266a61d79bb27a4902b820da64ab85
Joe Dumford <dumford.joe@gmail.com>
2019-05-12 17:56:41 -0400

    It's not pretty but it works

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+module.exports =  [
+    {
+        name: "Hank",
+        photo: "https://sites.psu.edu/siowfa16/files/2016/10/YeDYzSR-10apkm4.png",
+        // photo: "https://scontent.fatl1-1.fna.fbcdn.net/v/t31.0-8/1617720_307251952812061_841538114792036347_o.jpg?_nc_cat=109&_nc_ht=scontent.fatl1-1.fna&oh=2a6942b00f6aa6c1591bdd702df3e266&oe=5D5EFD5C",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
